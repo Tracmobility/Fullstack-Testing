0e7547acf03063a4b0e3563642272354d93886c7
Seyit Bahadir Cetin <seyitbahadirr@gmail.com>
2021-09-09 13:58:27 +0300

    -Material UI added to the project. -Sidebar navigation is implemented and added as component. -Datatable is created as component with Material UI datagrid. -Sorting, page size options, pagination added to the data table.

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className='about'>
+            <h1>Tracmobility Technical Test</h1>
+            <h3>Seyit Bahadir Cetin</h3>
+            <h4>09/09/2021</h4>          
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DataTable from '../components/DataTable'
+
+function Home() {
+    return (
+        <div className='home'>
+             <DataTable/>           
+        </div>
+    )
+}
+
+export default Home
